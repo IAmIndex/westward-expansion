c1ec798fb3d8b86b1e1d0b1859be872ef58b076e
IAmIndex <116038455+IAmIndex@users.noreply.github.com>
2023-10-28 00:51:38 -0300

    Functional (#1)
    * Random events modal functional
    
    * Gamble system made
    
    * Fixed css path
    
    * Fixed events formatting and added new events

--- javascript/modal.js
+++ javascript/modal.js
@@ -1,75 +1,115 @@
 const modal = document.getElementById("mainModal");
-const modalHeader = document.getElementById("card-header");
-const modalBody = document.getElementById("card-body");
-const modalFooter = document.getElementById("card-footer");
+const modalText = document.getElementById("modal-text");
+const modalHeader = document.getElementById("modal-title");
 
-const openingBtns = document.getElementsByClassName("btn-question-gen");
-const closingBtn = document.getElementById("closeModalBtn");
+const openingBtns = document.getElementsByClassName("card-button");
+const closingBtn = document.getElementById("modal-closing-button");
 
-let seenQuestions = {"theme-1":[],"theme-2":[],"theme-3":[],"theme-4":[]};
+const gambledValue = document.getElementById("text-input")
 
-let configsJsonFile;
-fetch('https://iamindex.github.io/Quiz-game/json/questions.json')
+let eventsJsonFile;
+fetch('/json/events.json')
     .then( (response)=> {return response.json()} )
-    .then(json=> configsJsonFile=json);
+    .then(json=> eventsJsonFile=json);
 
-const fetchQuestion = (theme,questionsArray) => {
-    const randomNumber = Math.floor(Math.random()*questionsArray.length); //Random number for an element from the array
-    const randomQuestion = questionsArray[randomNumber];
-    
-    if (seenQuestions[theme].length == 10) { //If all questions were chosen, clear the array and restart
-        seenQuestions[theme].splice(0,seenQuestions[theme].length);
-        seenQuestions[theme].push(randomQuestion)
-        return {"question":randomQuestion,"usedNumber":randomNumber}; 
-    } else if (seenQuestions[theme].includes(randomQuestion)) {
-        return fetchQuestion(theme,questionsArray);
-    } else {
-        seenQuestions[theme].push(randomQuestion)
-        return {"question":randomQuestion,"usedNumber":randomNumber};
+/**
+ * Fetches a random event
+ * 
+ * @param {string[]} eventsArray The array containing the events
+ * @returns {string} The random event
+ */
+const fetchEvent = (eventsArray) => {
+    const randomNumber = Math.floor(Math.random()*eventsArray.length); //Random number for an element from the array
+    const randomEvent = eventsArray[randomNumber];
+
+    return randomEvent;
+}
+
+/**
+ * Sets up the string with the event
+ * 
+ * @param {string} eventsArray The JSON array with the events
+ * @returns {string} The formatted question
+ */
+const setUpEvent = (eventsArray) => {
+    const randomEvent = fetchEvent(eventsArray);
+    const positiveEventPattern = /(\+)([^\+]+)(\+)/g;
+    const negativeEventPattern = /(-)([^-]+)(-)/g;
+    const regularEventPattern = /(~)([^~]+)(~)/g;
+
+    const isPositiveEvent = randomEvent.match(positiveEventPattern)
+
+    formattedQuestion = randomEvent.replace(negativeEventPattern,"<span style='color:#ff0000;'>$2</span>");
+    formattedQuestion = formattedQuestion.replace(positiveEventPattern,"<span style='color:#00ff00;'>$2</span>");
+    formattedQuestion = formattedQuestion.replace(regularEventPattern,"<span style='color:#ffbf00;'>$2</span>");
+
+    formattedQuestion = "<p id='modal-text'>" + formattedQuestion + "</p>";
+
+    return formattedQuestion;
+}
+
+/**
+ * Sets up the gamble values and results
+ * 
+ * @param {int} value The inserter value to gamble
+ * @returns {string: bool, string: int}
+ */
+const setUpGamble = (value) => {
+    value = value===0?100000000000:value;
+    const k = 100;
+    const winningChance = (k/value);
+    const randomNumber = Math.floor(Math.random()*100)+1;
+
+    const multiplier = 5;
+    const reward = multiplier*value;
+
+    if (randomNumber<=winningChance) {
+        return {
+            hasWon: true,
+            wonAmount: reward 
+        }
+    }
+
+    return {
+        hasWon: false,
+        wonAmount: 0
     }
 }
 
+/**
+ * 
+ * @param {event} e The onclick event
+ */
 const openModal = (e) => {
     //Cleaning the modals
-    modalHeader.innerHTML = "";
-    modalBody.innerHTML = "";
-    modalFooter.innerHTML = "";
 
     let currButton = e.currentTarget; //Button clicked
-    let buttonThemeNumber = currButton.dataset.targetTheme; //Theme from the Element
-    let theme = configsJsonFile[buttonThemeNumber]; //Theme from Element in the JSON
-
-    //From JSON file
-    let questionsArray = theme.questions;
-    let answersArray = theme.answers;
-    let difficultiesArray = theme.difficulty;
-
-    const generatedRandoms = fetchQuestion(buttonThemeNumber,questionsArray); //Random element an number
-    const randomNumber = generatedRandoms.usedNumber; //Random number
-    //Header
-    const randomQuestion = generatedRandoms.question; //Random element from the array
-    modalHeader.innerHTML = "<span class='close' onclick=\"document.getElementById('mainModal').style.display = 'none'\">&times;</span><h3>"+randomQuestion+"</h3>"; //Displays it
+    let buttonTheme = currButton.dataset.theme; //Theme from the Element
     
-    //Body
-    const pattern = /^\=/; //Pattern to the right answer
-    answersArray = shuffle(answersArray[randomNumber]); //Respective answers to question
-    for (let i=0;i<answersArray.length;i++) {
-        let answer = answersArray[i]
-        if (pattern.test(answersArray[i])) { //If is the correct answer
-            answer = answer.slice(1);
-            modalBody.innerHTML += "<p class='answer'>"+(i+1)+". "+answer+"</p><br>"; //Displays it
-            continue;
+    if (buttonTheme === "event") {
+        const formattedEvent = setUpEvent(eventsJsonFile);
+        modalHeader.innerHTML = "O que será que vai acontecer agora?";
+        modalText.innerHTML = formattedEvent;
+    } else {
+        const hasWonAndAmount = setUpGamble(parseInt(gambledValue.value));
+        if (hasWonAndAmount.hasWon) {
+            modalHeader.innerHTML = "Quais as chances?";
+            modalText.innerHTML = "<p id='modal-text'>Parabéns! Você <span style='color:#00ff00;'>ganhou $"+hasWonAndAmount.wonAmount+".000</span>! Recolha seu dinheiro com o banco";
+        } else {
+            modalHeader.innerHTML = "Você me deve!";
+            modalText.innerHTML = "<p id='modal-text'>Que pena! Parece que <span style='color:#ff0000'>você perdeu a aposta</span>. Não que isso seja um problema, você pode sempre tentar apostar de novo e ganhar mais do que perdeu! <span style='color:#ffbf00;'>Você pode ganhar 5x o valor inserido!</span>"
         }
-        modalBody.innerHTML += "<p class='answer-wrong'>"+(i+1)+". "+answer+"</p><br>"; //Displays it
     }
 
-    //Footer
-    const randomDifficulty = difficultiesArray[randomNumber]; //Random element from the array
-    modalFooter.innerHTML = "<h3>"+randomDifficulty+"</h3>"; //Displays it
-
-    modal.style.display = "block"; //Displays modal
+    modal.style.display = "flex"; //Displays modal
 }
 
+/**
+ * Shuffles the given array
+ * 
+ * @param {any[]} array The array to get shuffled
+ * @returns {any[]} The shuffled array
+ */
 function shuffle(array) {
     let currentIndex = array.length,  randomIndex;
   
@@ -85,11 +125,15 @@ function shuffle(array) {
     }
   
     return array;
-  }
+}
 
 for (let i=0;i<openingBtns.length;i++) {
     let currButton = openingBtns[i];
     currButton.addEventListener('click',openModal);
+}
+
+closingBtn.onclick = () => {
+    modal.style.display = "none";
 }
 
 window.onclick = (event) => {
